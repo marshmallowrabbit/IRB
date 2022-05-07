e280a88da8a4317f138943cfece91f7235e3d432
marshmallowrabbit <96782766+marshmallowrabbit@users.noreply.github.com>
2022-05-07 19:55:21 +0200

    Update Strategy.py

--- Strategy.py
+++ Strategy.py
@@ -24,7 +24,7 @@ def Analysis(file_name):
     df['sl'] = np.where((df['High'].values>df['y'].values) & (df['Close'].values<df['y'].values) & (df['Open'].values<df['y'].values),1,0)
     df['ss'] = np.where((df['Low'].values<df['x'].values) & (df['Close'].values>df['x'].values) & (df['Open'].values>df['x'].values),1,0)
     df['hc'] = df.High - df.Low
-    df['hcs'] = abs(df.High-df.Close.shift(1))#.shift(1)
+    df['hcs'] = abs(df.High-df.Close.shift(1))
     df['lcs'] = abs(df.Low-df.Close.shift(1))
     df['rt'] = df[['hc','hcs','lcs']].max(axis=1)
     df['slow'] = ta.sma(df['Close'],5)
@@ -34,6 +34,8 @@ def Analysis(file_name):
     df['trend 3'] = ta.ema(df['Close'],144)
     df['mid'] = ta.ema(df['Close'],35)
     df['low'] = ta.ema(df['Close'],35) + 0.5*(ta.rma(df['rt'],35))
+    df['longOn'] = np.where((df.slow.values>df.fast.values)&(df['trend 1'].values<df.fast.values)&(df['trend 2'].values<df.fast.values)&(df['trend 3'].values<df.fast.values)&(df['mid'].values<df.fast.values)&(df['low'].values<df.fast.values),1,0)
+    df['shortOn'] = np.where((df.slow.values<df.fast.values)&(df['trend 1'].values>df.fast.values)&(df['trend 2'].values>df.fast.values)&(df['trend 3'].values>df.fast.values)&(df['mid'].values>df.fast.values)&(df['low'].values>df.fast.values),1,0)
     return df
 
 print(Analysis(ohlc))
