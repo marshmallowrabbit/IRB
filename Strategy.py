015ed9284e8f46231a05aa1de7f06a3d60f8d894
marshmallowrabbit <96782766+marshmallowrabbit@users.noreply.github.com>
2022-05-07 04:25:46 +0200

    Update Strategy.py

--- Strategy.py
+++ Strategy.py
@@ -23,14 +23,14 @@ def Analysis(file_name):
     df['y'] = df.apply(lambda row: row.High - row.ca, axis = 1)
     df['sl'] = np.where((df['High'].values>df['y'].values) & (df['Close'].values<df['y'].values) & (df['Open'].values<df['y'].values),1,0)
     df['ss'] = np.where((df['Low'].values<df['x'].values) & (df['Close'].values>df['x'].values) & (df['Open'].values>df['x'].values),1,0)
-    print(help(ta.sma(df.Close)))
-    df['slow'] = df.ta.sma(close=df['Close'].values,length=5)
-    # df['fast'] = df.ta.ema(df['Close'].values,18)
-    # df['trend 1'] = df.sma(df['Close'].values,50)
-    # df['trend 2'] = df.sma(df['Close'].values,89)
-    # df['trend 3'] = df.ema(df['Close'].values,144)
-    # df['mid'] = df.ema(df['Close'].values,35)
-    # df['low'] = df.ema(df['Close'].values,35)# + 0.5*(df.ta.rma)
+    df['rt'] = 
+    df['slow'] = ta.sma(df['Close'],5)
+    df['fast'] = ta.ema(df['Close'],18)
+    df['trend 1'] = ta.sma(df['Close'],50)
+    df['trend 2'] = ta.sma(df['Close'],89)
+    df['trend 3'] = ta.ema(df['Close'],144)
+    df['mid'] = ta.ema(df['Close'],35)
+    df['low'] = ta.ema(df['Close'],35) + 0.5*(ta.rma(df[''],35)
 
     return df
 
