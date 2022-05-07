a1766ebfcc4d4e93279575d74134cdef1941b89e
marshmallowrabbit <96782766+marshmallowrabbit@users.noreply.github.com>
2022-05-07 04:57:09 +0200

    Update Strategy.py

--- Strategy.py
+++ Strategy.py
@@ -14,24 +14,25 @@ def Analysis(file_name):
     riskratio = 1.5
     print('Analyzing...')
     df = pd.read_csv(file_name)
-    print(df)
     df['a'] = abs(df.apply(lambda row: row.High - row.Low, axis=1))
     df['b'] = abs(df.apply(lambda row: row.Close - row.Open, axis=1))
-    df['ca'] = df.apply(lambda row: row.a * c, axis = 1)
+    df['ca'] = df.apply(lambda row: row.a * c, axis=1)
     df['rv'] = np.where(df['b'].values < df['ca'].values,1,0)
-    df['x'] = df.apply(lambda row: row.Low + row.ca, axis = 1)
-    df['y'] = df.apply(lambda row: row.High - row.ca, axis = 1)
+    df['x'] = df.apply(lambda row: row.Low + row.ca, axis=1)
+    df['y'] = df.apply(lambda row: row.High - row.ca, axis=1)
     df['sl'] = np.where((df['High'].values>df['y'].values) & (df['Close'].values<df['y'].values) & (df['Open'].values<df['y'].values),1,0)
     df['ss'] = np.where((df['Low'].values<df['x'].values) & (df['Close'].values>df['x'].values) & (df['Open'].values>df['x'].values),1,0)
-    df['rt'] = 
+    df['hc'] = df.apply(lambda row: df['High']-df['Low'],axis=1)
+    df['hcs'] = df.apply(lambda row: abs(df.High-df.Close),axis=1)#.shift(1)
+    df['lcs'] = df.apply(lambda row: abs(df.Low-df.Close),axis=1)
+    df['rt'] = df[['hc','hcs','lcs']].max(axis=1)
     df['slow'] = ta.sma(df['Close'],5)
     df['fast'] = ta.ema(df['Close'],18)
     df['trend 1'] = ta.sma(df['Close'],50)
     df['trend 2'] = ta.sma(df['Close'],89)
     df['trend 3'] = ta.ema(df['Close'],144)
     df['mid'] = ta.ema(df['Close'],35)
-    df['low'] = ta.ema(df['Close'],35) + 0.5*(ta.rma(df[''],35)
-
+    df['low'] = ta.ema(df['Close'],35) + 0.5*(ta.rma(df['rt'],35))
     return df
 
 print(Analysis(ohlc))
