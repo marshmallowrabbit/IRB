ad09cfe6a232cb2451cf701c24daf4fb1566924e
marshmallowrabbit <96782766+marshmallowrabbit@users.noreply.github.com>
2022-05-18 22:35:49 +0100

    Add files via upload

---
+++ recallFunctions.py
@@ -0,0 +1,11 @@
+import os
+
+def recall(data_file):
+    data_file_ent = []
+    for file in os.listdir(data_file):
+        if file.endswith('.csv'):
+            data_file_ent.append(file)
+    for file in data_file_ent:
+        if str(1) in file:
+            df = pd.read_csv(data_file+'/'+'BTC'+str(1)+'.csv')
+    return df
