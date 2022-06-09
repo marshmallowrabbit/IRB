ca8187eae29ee8ec490af81544d8dfb7f414408f
marshmallowrabbit <96782766+marshmallowrabbit@users.noreply.github.com>
2022-06-09 22:35:48 +0200

    Update Signal.py

--- Red/Signal.py
+++ Red/Signal.py
@@ -1,37 +1,36 @@
 def signals(df):
     Longs = []
     ExitLongs = []
-    temp_long_stop = []
-    temp_long_target = []
-    # temp_long_stop = 0
-    # temp_long_target = 0
-    for i in range(len(df)):
-        if 'y' in df['longOn'].iloc[i] and 'n' in df['longOn'].iloc[i-1]:
-            if df['High'].iloc[i]>df['longlimit'].iloc[i]:
-                Longs.append(df.iloc[i].name)
-                # temp_long_stop = df.iloc[i].longstop.values
-                # temp_long_target = df.iloc[i].longtarget.values
-                temp_long_stop.append(df.iloc[i].longstop)
-                temp_long_target.append(df.iloc[i].longtarget)
-            # for j in range(len(df)-i):
-            #     if df[''].iloc[i+j]<:
-            #         ExitLongs.append(df.iloc[i+j].name)       
-    return Longs,ExitLongs,temp_long_stop,temp_long_target
-
-def signals(df):
-    Longs = []
-    ExitLongs = []
-    temp_long_stop = 0
-    temp_long_target = 0
+    Shorts = []
+    ExitShorts = []
+#     temps
+    temp_longstop = 0
+    temp_longtarget = 0
+    temp_shortstop = 0
+    temp_shorttarget = 0
+#     nested for loop
     for i in range(len(df)):
         if 'y' in df['longOn'].iloc[i] and 'n' in df['longOn'].iloc[i-1] and df['High'].iloc[i]>df['longlimit'].iloc[i]:
-            Longs.append(df.iloc[i].name)
-            temp_long_stop = df.iloc[i].longstop
-            temp_long_target = df.iloc[i].longtarget
-        for j in range(len(df)-i):
-            if df['Low'].iloc[i+j]<temp_long_stop or df['High'].iloc[i+j]>temp_long_target:
-                ExitLongs.append(df.iloc[i+j].name)
-            temp_long_stop = 0
-            temp_long_target = 0
-    return Longs,ExitLongs
-l,el = signals(df)
+            Longs.append(df.iloc[i].Timestamp)
+            temp_longstop = df.iloc[i].longstop
+            temp_longtarget = df.iloc[i].longtarget
+            for j in range(len(df)-i):
+                if df['Low'].iloc[i+j]<temp_longstop or df['High'].iloc[i+j]>temp_longtarget:
+                    ExitLongs.append(df.iloc[i+j].Timestamp)
+                    temp_longstop = 0
+                    temp_longtarget = 0
+                    break
+    for i in range(len(df)):
+        if 'y' in df['shortOn'].iloc[i] and 'n' in df['shortOn'].iloc[i-1] and df['Low'].iloc[i]<df['shortlimit'].iloc[i]:
+            Shorts.append(df.iloc[i].Timestamp)
+            temp_shortstop = df.iloc[i].longstop
+            temp_shorttarget = df.iloc[i].longtarget
+            for j in range(len(df)-i):
+                if df['Low'].iloc[i+j]<temp_shorttarget or df['High'].iloc[i+j]>temp_shortstop:
+                    ExitShorts.append(df.iloc[i+j].Timestamp)
+                    temp_shortstop = 0
+                    temp_shorttarget = 0
+                    break
+    
+    return Longs,ExitLongs,Shorts,ExitShorts
+l,el,s,es = signals(df)
