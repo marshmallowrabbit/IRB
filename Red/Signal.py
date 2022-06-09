10a0a6bc098b69d7a2b0a8fa5497e2cca878458f
marshmallowrabbit <96782766+marshmallowrabbit@users.noreply.github.com>
2022-06-09 14:06:35 +0200

    Update Signal.py

--- Red/Signal.py
+++ Red/Signal.py
@@ -1,13 +1,19 @@
 def signals(df):
     Longs = []
-    Shorts = []
     ExitLongs = []
-    ExitShorts = []
+    temp_long_stop = []
+    temp_long_target = []
+    # temp_long_stop = 0
+    # temp_long_target = 0
     for i in range(len(df)):
-        if 'y' in df['longOn'].iloc[i] and 'n' in df['longOn'].iloc[i-1] and inPosition == 0:
+        if 'y' in df['longOn'].iloc[i] and 'n' in df['longOn'].iloc[i-1]:
             if df['High'].iloc[i]>df['longlimit'].iloc[i]:
                 Longs.append(df.iloc[i].name)
-            for j in range range(len(df)-i):
-                if 'y' in df['longOff'].iloc[i+j] and 'n' in df['longOff'].iloc[i+j-1]:
-                    ExitLongs.append(df.iloc[i+j].name)       
-    return Longs,ExitLongs
+                # temp_long_stop = df.iloc[i].longstop.values
+                # temp_long_target = df.iloc[i].longtarget.values
+                temp_long_stop.append(df.iloc[i].longstop)
+                temp_long_target.append(df.iloc[i].longtarget)
+            # for j in range(len(df)-i):
+            #     if df[''].iloc[i+j]<:
+            #         ExitLongs.append(df.iloc[i+j].name)       
+    return Longs,ExitLongs,temp_long_stop,temp_long_target
