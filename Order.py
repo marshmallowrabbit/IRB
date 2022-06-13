a0e921b0227f1a16381a97402cb5579b1da7da00
marshmallowrabbit <96782766+marshmallowrabbit@users.noreply.github.com>
2022-06-13 21:23:17 +0200

    Update Order.py

--- Order.py
+++ Order.py
@@ -7,10 +7,38 @@ ftx = ccxt.ftx({
         "FTX-SUBACCOUNT": "TestAccount"
     }
 })
-    
-def Order(trade_type,limit_price,stop_price,target_price):
-    ftx.createOrder()
-    ftx.createStopLimitOrder()
-    ftx.createStopOrder()
-    params = {}
-    setLeverage (leverage, symbol = undefined, params = {})
+
+ftx.load_markets()
+
+symbol = 'ETH-PERP'
+type = 'limit'
+side = 'buy'
+amount = 0.004
+price = 1200
+params = {
+    'stopPrice': 1150,
+    # 'ordType': 'StopLimit',
+    'takeProfit':1300
+}
+order = ftx.create_order(symbol, type, side, amount, price, params)
+
+
+# ethPrice = 1000
+# stopPrice = 5000
+# trailingOrder = exchange.create_order(symbol, type = 'StopLimit', side, amount, price, params = {
+#     'stopPrice': 1150,
+#     'ordType': 'StopLimit',
+#     'pegPriceType': 'TrailingStopPeg',
+#     'pegOffsetValueEp': 10000, # needs to be scaled
+#     'triggerType': 'ByMarkPrice'
+#     })
+
+
+
+# def Order(trade_type,limit_price,stop_price,target_price):
+#     ftx.createOrder()
+#     ftx.createStopLimitOrder()
+#     ftx.createStopOrder()
+#     params = {}
+#     setLeverage (leverage, symbol = undefined, params = {})
+
