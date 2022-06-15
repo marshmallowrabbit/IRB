834d4d13b860155806a3767c71025ee23031d9bf
marshmallowrabbit <96782766+marshmallowrabbit@users.noreply.github.com>
2022-06-15 19:47:48 +0200

    Update Order.py

--- Order.py
+++ Order.py
@@ -8,30 +8,28 @@ ftx = ccxt.ftx({
     }
 })
 
-ftx.load_markets()
-
 symbol = 'ETH-PERP'
-type_1 = 'limit'
-type_2 = 'StopLimit'
-type_3 = 'TakeProfitLimit'
-side_1 = 'buy'
-side_2 = 'sell'
+order_type = 'limit'
+# type_2 = 'StopLimit'
+# type_3 = 'TakeProfitLimit'
+order_side = 'buy'
+# side_2 = 'sell'
 amount = 0.004
-price = 1206
-stop_price = 1200
-takeProfit_price = 1212
-# ftx.cancel_all_orders(symbol,params={'conditionalOrdersOnly': True})
-# ftx.cancel_all_orders(symbol)
-# order = ftx.create_order(symbol, type_1, side_1, amount, price)
-# stoploss = ftx.create_order(symbol, type_2, side_2, amount, stop_price, params = {
-#     'stopPrice': stop_price,
-#     'ordType': 'StopLimit'
-#     })
-# takeprofit = ftx.create_order(symbol, type_3, side_2, amount, takeProfit_price, params = {
-#     'takeProfitPrice': takeProfit_price,
-#     'ordType': 'TakeProfitLimit'
-#     })
+price = 1082
+stop_price = 1000
+takeProfit_price = 1280
 
+
+def Order(symbol,order_type,order_side,amount,price,stop_limit,target_limit):
+    ftx.load_markets()
+    ftx.cancel_all_orders(symbol,params={'conditionalOrdersOnly': True})
+    ftx.cancel_all_orders(symbol)
+    
+    order = ftx.create_order(symbol, type = order_type, side = order_side, amount = amount, price = price)
+    stoploss = ftx.create_order(symbol, type = 'StopLimit', side = 'sell', amount = amount, price = stop_price, params = {'stopPrice': stop_price,'ordType': 'StopLimit'})
+    takeprofit = ftx.create_order(symbol, type = 'TakeProfitLimit', side = 'sell', amount = amount, price = takeProfit_price, params = {'takeProfitPrice': takeProfit_price,'ordType': 'TakeProfitLimit'})
+
+Order(symbol,order_type,order_side,amount,price,stop_price,takeProfit_price)
 
 # THIS CODE RESULTS IN A STOP LOSS AND TAKE PROFIT TRIGGER ORDER HOWEVER, IF STOPLOSS IS TRIGGERED THEN TAKEPROFIT REMAINS AND VICE-VERSA
 
